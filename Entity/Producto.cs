77ce7babd00618a83e6f7ee32ccec4842659a76a
yammaru <63098405+yammaru@users.noreply.github.com>
2020-05-04 08:11:32 -0500

    Add files via upload

---
+++ Entity/Producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Producto
+    {
+        public string CodigoProducto { set; get; }
+        public string NombreProducto { set; get; }
+        public int Cantidad { set; get; }      
+        public string Estado { set; get; }
+        public DateTime Fecha { set; get; }
+        public Producto()
+        {
+           
+        }
+
+        public Producto(string codigoProducto, string nombreProducto, int cantidad, string estado, DateTime fecha)
+        {
+            CodigoProducto = codigoProducto;
+            NombreProducto = nombreProducto;
+            Cantidad = cantidad;
+            Estado = estado;
+            Fecha = fecha;
+        }
+
+        public override string ToString()
+        {
+            return $"{CodigoProducto};{NombreProducto};{Cantidad};{Estado};{Fecha}";
+        }
+    }
+}
